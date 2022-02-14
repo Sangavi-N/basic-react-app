bdf32b5eff54fc22bd00d6d49e809c71906a14cd
aravindhs31 <aravindh.acumen@gmail.com>
2022-02-14 16:54:30 +0530

    branch

--- src/index.js
+++ src/index.js
@@ -3,10 +3,30 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from './components/header'
+import Footer from './components/footer'
+import About from './components/about'
+import Service from './components/service'
+
+const Routing = () => {
+  return(
+    <Router>
+      <Header/>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/about" component={About} />
+        <Route path="/service" component={Service} />
+      </Switch>
+      <Footer/>
+    </Router>
+  )
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Routing />
   </React.StrictMode>,
   document.getElementById('root')
 );
